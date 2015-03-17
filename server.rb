93d0559f4ff060c8f0fb51b0fe644d57194044b4
Rob Monk <robert.monk85@gmail.com>
2015-03-17 15:08:47 -0400

    initial commit

--- server.rb
+++ server.rb
@@ -1,12 +1,20 @@
 require 'httparty'
 require 'sinatra'
+require 'dotenv'
 require 'pry'
-# https://github.com/SpencerCDixon/API-
-# api key = a72ccb932deb3d5bc1e425bfde4cffed
-
+Dotenv.load
 
 def get_data
-  http = "https://outdoor-data-api.herokuapp.com/api.json?api_key=a72ccb932deb3d5bc1e425bfde4cffed"
+  api = ENV['API_KEY']
+  http = "https://outdoor-data-api.herokuapp.com/api.json?api_key=#{api}&limit=10"
+  json = HTTParty.get(http)
+  json
+end
+
+def get_data2(search_string)
+  api = ENV['API_KEY']
+  string = search_string.gsub( '@$', '/' )
+  http = "https://outdoor-data-api.herokuapp.com/api.json?api_key=#{api}#{string}"
   json = HTTParty.get(http)
   json
 end
@@ -14,4 +22,27 @@ end
 get '/' do
   campsites = get_data
   erb :index, locals: { campsites: campsites }
+end
+
+post '/' do
+  state = params["state"].gsub(" ", "%20")
+  activity = params["activity"].gsub(" ", "%20")
+
+  redirect '/search/' + state + '/' + activity
+end
+
+get '/search/:state/:activity' do
+  state = params[:state].gsub(" ", "%20")
+  activity = params[:activity].gsub(" ", "%20")
+  search_string = "&q[state_eq]=#{state}"
+  search_string += "&q[activities_activity_type_name_cont]=#{activity}"
+  campsites = get_data2(search_string)
+  erb :index, locals: { campsites: campsites }
+end
+
+get '/:name' do
+  name = params[:name].gsub(" ", "%20")
+  search_string = "&q[name_eq]=#{name}"
+  campsites = get_data2(search_string)
+  erb :show, locals: { campsites: campsites }
 end
